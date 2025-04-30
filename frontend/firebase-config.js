d485e5ea1c6364f58ff6a06fffb26786aba5ba52
anujthakur-10 <anuj10122004@gmail.com>
2025-04-30 21:55:53 +0530

    Add files via upload

---
+++ frontend/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBduirdtq8s5FSi0ujPvzEva7cajvBbMko",
+    authDomain: "my-ai-app-bb622.firebaseapp.com",
+    projectId: "my-ai-app-bb622",
+    storageBucket: "my-ai-app-bb622.firebasestorage.app",
+    messagingSenderId: "849466789539",
+    appId: "1:849466789539:web:96e9b690d2b0ebcbc0b916"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  
